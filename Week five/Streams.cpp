2fa67f568dab723020f56d4942da53da370eea15
Lucia Zuniga <Zuniga00lucia@gmail.com>
2025-11-29 21:34:52 -0500

    Add Streams.cpp for basic I/O operations
    Implement basic input/output using streams

---
+++ Week five/Streams.cpp
@@ -0,0 +1,21 @@
+//STREAMS
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int number;
+
+    cout << "Enter a number: " << endl;
+    cin >> number;
+    cout << "You entered: " << number << endl;  
+
+    cerr << "This is an error message." << endl; //This is an error message | warning
+    clog << "This is a log message.\n" << endl; //This is a log message | notes
+    
+    
+
+
+
+
+}
