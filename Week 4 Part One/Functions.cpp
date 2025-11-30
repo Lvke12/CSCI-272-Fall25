e445e965a97b835ab3417bbcc6ca8341ff7567eb
Lucia Zuniga <Zuniga00lucia@gmail.com>
2025-11-29 21:25:44 -0500

    Create Functions.cpp

---
+++ Week 4 Part One/Functions.cpp
@@ -0,0 +1,20 @@
+using namespace std;
+#include <iostream>
+int main() {
+    double num1, num2;
+
+    cout<<"Enter the first number: ";
+    cin>>num1;
+
+    cout<<"Enter the second number: ";
+    cin>>num2;
+
+
+    double sum = num1 + num2;
+    double quotient = num1 / num2;
+
+    cout<<"The sum is: "<<sum<<endl;
+    cout<<"The quotient is: "<<quotient<<endl;
+    return 0;
+
+}
